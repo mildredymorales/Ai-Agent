418edc47153d581ba3287f1183e0d2628e07de5b
selnsimay <122891354+selnsimay@users.noreply.github.com>
2024-07-10 15:01:33 -0400

    Update task10_exp.py

--- Experiments/task10_exp.py
+++ Experiments/task10_exp.py
@@ -19,55 +19,55 @@ print('-------------------------------')
 genes = 'GPR153, TMEM100, FRMPD1, ANKRD22, TMEM86A, UNC80, CLIC2, STEAP4, TROAP, DLGAP5, KIF20A, KIF18B, KIF14,  PRSS12, RNASE1, RNASE4, TFPI2, GBP5, OASL, AGR2, CFAP70, METRNL, MANSC4'
 
 # Define your custom agents here
-custom_agent_1 = agents.cellular_biologist()
-custom_agent_2 = agents.computational_biologist()
-custom_agent_3 = agents.cell_cyc()
-custom_agent_4 = agents.drug_dev()
-custom_agent_5 = agents.epigenetics_biologist()
-custom_agent_6 = agents.genereg_expert()
-custom_agent_7 = agents.molecular_biologist()
-custom_agent_8 = agents.onco_res()
-custom_agent_9 = agents.oncologist_physician()
-custom_agent_10 = agents.systems_biologist()
+cell_biol = agents.cellular_biologist()
+comp_biol = agents.computational_biologist()
+cell_exp = agents.cell_cyc()
+drug_devo = agents.drug_dev()
+epi_biol = agents.epigenetics_biologist()
+genereg_expe = agents.genereg_expert()
+mol_biol = agents.molecular_biologist()
+onco_rese = agents.onco_res()
+onco_phys = agents.oncologist_physician()
+systems_biol = agents.systems_biologist()
 
 
 custom_agents = [
-    custom_agent_1, custom_agent_2, custom_agent_3,custom_agent_3,custom_agent_5,custom_agent_6,custom_agent_7,custom_agent_8, custom_agent_9,custom_agent_10
+    cell_biol, comp_biol, cell_exp,drug_devo,epi_biol,genereg_expe,mol_biol,onco_rese, onco_phys,systems_biol
 ]
 
 # Define your custom tasks here
-custom_task_1 = tasks10.immune_response(
+imm_resp = tasks10.immune_response(
     custom_agents, genes,
 )
-custom_task_2 = tasks10.metabolic_process(
+meta_proc = tasks10.metabolic_process(
     custom_agents, genes,
 )
-custom_task_3 = tasks10.cell_cycle(
+cell_cycl = tasks10.cell_cycle(
     custom_agents, genes,
 )
-custom_task_4 = tasks10.cell_comm(
+cell_commu = tasks10.cell_comm(
     custom_agents, genes,
 )
-custom_task_5 = tasks10.signal_trans(
+sig_trans = tasks10.signal_trans(
     custom_agents, genes,
 )
-custom_task_6 = tasks10.apop(
+apopt = tasks10.apop(
     custom_agents, genes,
 )
-custom_task_7 = tasks10.develop(
+devel = tasks10.develop(
     custom_agents, genes,
 )
-custom_task_8 = tasks10.repro(
+reprod = tasks10.repro(
     custom_agents, genes,
 )
-custom_task_9 = tasks10.transport(
+transp = tasks10.transport(
     custom_agents, genes,
 )
-custom_task_10 = tasks10.reg_biop(
+regu_biop = tasks10.reg_biop(
     custom_agents, genes,
 )
 
-task_definitions = [ custom_task_1, custom_task_2, custom_task_3, custom_task_4, custom_task_5, custom_task_6, custom_task_7, custom_task_8, custom_task_9, custom_task_10
+task_definitions = [ imm_resp, meta_proc, cell_cycl, cell_commu, sig_trans, apopt, devel, reprod, transp, regu_biop
 ]
 
 crew = Crew(
