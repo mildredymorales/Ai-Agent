b7e519b428bb3ae2c292b0bd5a8b68c6cd1a5855
mildredymorales <mmoralesparedes@g.hmc.edu>
2024-07-12 10:10:25 -0400

    edit anna example to run all terms at once for each agent as a dict, edit the term task to handle this

--- Experiments/anna_ex.py
+++ Experiments/anna_ex.py
@@ -12,9 +12,24 @@ from Source_Code.tasks import BiologicalAnalysisTask
 agents = BiologicalAgents()
 tasks = BiologicalAnalysisTask()
 
-genes = input("Insert the genes to be analyzed as a list?\n")
+# genes = input("Insert the genes to be analyzed as a list?\n")
+# term = input("What is the the biological term you want it to specifically analyze as?\n")
 
-term = input("What is the the biological term you want it to specifically analyze as?\n")
+genes = 'GBP5, OASL, ANKRD22, AGR2, LGALS4, KIF14, KIF20A, KIF18B, DLGAP5, TROAP, DEPDC1, PRR11'
+
+term = {
+    "Immune response": "Immune response",
+    "Metabolic process": "Metabolic process",
+    "Cell cycle": "Cell cycle",
+    "Cell communication": "Cell communication",
+    "Signal transduction": "Signal transduction",
+    "Apoptosis": "Apoptosis",
+    "Development": "Development",
+    "Reproduction": "Reproduction",
+    "Transport": "Transport",
+    "Regulation of biological processes": "Regulation of biological processes"
+}
+
 
 verbose_output = StringIO()
 sys.stdout = verbose_output
@@ -23,49 +38,67 @@ print("## Welcome to the Biology Crew")
 print('-------------------------------')
 
 # Define your custom agents and tasks here
-cell_bio_agent = agents.cellular_biologist()
-comp_bio_agent = agents.computational_biologist()
-cell_cyc_agent = agents.cell_cyc()
-drug_dev_agent = agents.drug_dev()
-epig_bio_agent = agents.epigenetics_biologist()
-gene_reg_agent = agents.genereg_expert()
-molec_bio_agent = agents.molecular_biologist()
-onco_res_agent = agents.onco_res()
-onco_phy_agent = agents.oncologist_physician()
-sys_bio_agent = agents.systems_biologist()
+# cell_bio_agent = agents.cellular_biologist()
+# comp_bio_agent = agents.computational_biologist()
+# cell_cyc_agent = agents.cell_cyc()
+# drug_dev_agent = agents.drug_dev()
+# epig_bio_agent = agents.epigenetics_biologist()
+# gene_reg_agent = agents.genereg_expert()
+# molec_bio_agent = agents.molecular_biologist()
+# onco_res_agent = agents.onco_res()
+# onco_phy_agent = agents.oncologist_physician()
+# sys_bio_agent = agents.systems_biologist()
 
+# analytical_agent = agents.compare_agent()
+
+# cell_bio_task = tasks.term(
+#     cell_bio_agent, genes, term
+# )
+# comp_bio_task = tasks.term(
+#     comp_bio_agent, genes, term
+# )
+# cell_cyc_task = tasks.term(
+#     cell_cyc_agent, genes, term
+# )
+# drug_dev_task = tasks.term(
+#     drug_dev_agent, genes, term
+# )
+# epig_bio_task = tasks.term(
+#     epig_bio_agent, genes, term
+# )
+# gene_reg_task = tasks.term(
+#     gene_reg_agent, genes, term
+# )
+# mol_bio_task = tasks.term(
+#     molec_bio_agent, genes, term
+# )
+# onco_res_task = tasks.term(
+#     onco_res_agent, genes, term
+# )
+# onco_phy_task = tasks.term(
+#     onco_phy_agent, genes, term
+# )
+# sys_bio_task = tasks.term(
+#     sys_bio_agent, genes, term
+# )
+
+
+agent_names = [
+    "cellular_biologist", "computational_biologist", "cell_cyc", "drug_dev",
+    "epigenetics_biologist", "genereg_expert", "molecular_biologist", 
+    "onco_res", "oncologist_physician", "systems_biologist"
+]
+
+
+agents_list = [agents.__getattribute__(name)() for name in agent_names]
 analytical_agent = agents.compare_agent()
 
-cell_bio_task = tasks.term(
-    cell_bio_agent, genes, term
-)
-comp_bio_task = tasks.term(
-    comp_bio_agent, genes, term
-)
-cell_cyc_task = tasks.term(
-    cell_cyc_agent, genes, term
-)
-drug_dev_task = tasks.term(
-    drug_dev_agent, genes, term
-)
-epig_bio_task = tasks.term(
-    epig_bio_agent, genes, term
-)
-gene_reg_task = tasks.term(
-    gene_reg_agent, genes, term
-)
-mol_bio_task = tasks.term(
-    molec_bio_agent, genes, term
-)
-onco_res_task = tasks.term(
-    onco_res_agent, genes, term
-)
-onco_phy_task = tasks.term(
-    onco_phy_agent, genes, term
-)
-sys_bio_task = tasks.term(
-    sys_bio_agent, genes, term
-)
+# Define your tasks here
+tasks_list = []
+for agent in agents_list:
+    tasks_list.append(tasks.term(agent, genes, term))
+
+compare_task = tasks.compare_findings(analytical_agent)
 
 
 compare_task = tasks.compare_findings(
@@ -73,18 +106,24 @@ compare_task = tasks.compare_findings(
 )
 
 # Define your custom crew here
-crew = Crew(
-        agents=[
-            cell_bio_agent, comp_bio_agent, cell_cyc_agent, drug_dev_agent, epig_bio_agent, gene_reg_agent, molec_bio_agent, onco_res_agent,
-            onco_phy_agent, sys_bio_agent, analytical_agent, 
-            ],
-        tasks=[
-            cell_bio_task, comp_bio_task, cell_cyc_task, drug_dev_task, epig_bio_task, gene_reg_task, mol_bio_task, onco_res_task, onco_phy_task, onco_phy_task, sys_bio_task, compare_task,
-            ],
-        verbose=True,
-        process = Process.sequential
-)
+# crew = Crew(
+#         agents=[
+#             cell_bio_agent, comp_bio_agent, cell_cyc_agent, drug_dev_agent, epig_bio_agent, gene_reg_agent, molec_bio_agent, onco_res_agent,
+#             onco_phy_agent, sys_bio_agent, analytical_agent, 
+#             ],
+#         tasks=[
+#             cell_bio_task, comp_bio_task, cell_cyc_task, drug_dev_task, epig_bio_task, gene_reg_task, mol_bio_task, onco_res_task, onco_phy_task, onco_phy_task, sys_bio_task, compare_task,
+#             ],
+#         verbose=True,
+#         process = Process.sequential
+# )
 
+crew = Crew(
+    agents=agents_list + [analytical_agent],
+    tasks=tasks_list + [compare_task],
+    verbose=True,
+    process=Process.sequential
+)
 
 
 result = crew.kickoff()
