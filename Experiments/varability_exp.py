7167eaf8057dff8ebc233f505c1a8962b5cd08d6
mildredymorales <mmoralesparedes@g.hmc.edu>
2024-07-17 15:01:42 -0400

    edit approach

--- Experiments/varability_exp.py
+++ Experiments/varability_exp.py
@@ -17,70 +17,85 @@ from Source_Code.tasks import BiologicalAnalysisTask
 agents = BiologicalAgents()
 tasks = BiologicalAnalysisTask()
 
-# genes = input("Insert the genes to be analyzed as a list?\n")
-# term = input("What are the the biological terms you want it to specifically analyze as? Enter the list with double quotes separated by commas. \n")
-agent = input("What are the the biological agents you want it to specifically analyze as? Enter the list with double quotes separated by commas. \n")
-task = input("What are the the biological terms you want it to specifically analyze as? Enter the list with double quotes separated by commas. \n")
-
-# biological_terms = [term.strip() for term in term.split(',')]
-
-# converting list into a dictionary 
-# term = {f"term {i+1}": term for i, term in enumerate(biological_terms)}
-
-# Printing the resulting dictionary
-# print(term)
-
-
+# approach of human input 
 print("## Welcome to the Biology Crew")
 print('-------------------------------')
 
+
+# agent = input("What is the the biological agent you want it to specifically analyze as? Please enter with NO quotes. For now it is sensitive so enter the exact function name.\n")
+# task = input("What is the the biological terms you want it to specifically analyze as? Please enter with NO quotes. For now it is sensitive so enter the exact function name\n")
+
+
+
+
 genes = 'GBP5, OASL, ANKRD22, AGR2, LGALS4, KIF14, KIF20A, KIF18B, DLGAP5, TROAP, DEPDC1, PRR11'
 
-# term = {
-#     "Immune response": "Immune response",
-#     "Metabolic process": "Metabolic process",
-#     "Cell cycle": "Cell cycle",
-#     "Cell communication": "Cell communication",
-#     "Signal transduction": "Signal transduction",
-#     "Apoptosis": "Apoptosis",
-#     "Development": "Development",
-#     "Reproduction": "Reproduction",
-#     "Transport": "Transport",
-#     "Regulation of biological processes": "Regulation of biological processes"
-# }
+# # agent = getattr(agents, agent)
+# # task = getattr(tasks, task)
+# agent = getattr(agents, agent)
+# task = getattr(tasks, task)(agent, genes)
 
-# make list of agents and tasks to loop over 
-agent_names = [
-    "cellular_biologist", "computational_biologist", "cell_cyc", "drug_dev",
-    "epigenetics_biologist", "genereg_expert", "molecular_biologist", 
-    "onco_res", "oncologist_physician", "systems_biologist"
-]
+# Define your custom agents and tasks here
+cell_bio_agent = agents.cellular_biologist()
+comp_bio_agent = agents.computational_biologist()
+cell_cyc_agent = agents.cell_cyc()
+drug_dev_agent = agents.drug_dev()
+epig_bio_agent = agents.epigenetics_biologist()
+gene_reg_agent = agents.genereg_expert()
+molec_bio_agent = agents.molecular_biologist()
+onco_res_agent = agents.onco_res()
+onco_phy_agent = agents.oncologist_physician()
+sys_bio_agent = agents.systems_biologist()
 
-agents_list = [agents.__getattribute__(name)() for name in agent_names]
 
-task_names = [
-    "apop", "cell_comm", "cell_cycle", "develop", "immune_response", 
-    "metabolic_process", "reg_biop", "repo", "signal_trans", "transport"
-]
+cell_cyc_task = tasks.cell_cycle(
+    cell_bio_agent, genes, 
+)
+# cell_com_task = tasks.cell_comm(
+#     agent, genes,
+# )
+# apop_task = tasks.apop(
+#     agent, genes, 
+# )
+# dev_task = tasks.develop(
+#     agent, genes, 
+# )
+# immune_res_task = tasks.immune_response(
+#     agent, genes, 
+# )
+# meta_pro_task = tasks.metabolic_process(
+#     agent, genes,
+# )
+# reg_bio_task = tasks.reg_biop(
+#     agent, genes, 
+# )
+# repro_task = tasks.repro(
+#     agent, genes, 
+# )
+# sig_tans_task = tasks.signal_trans(
+#     agent, genes, 
+# )
+# transport_task = tasks.transport(
+#     agent, genes,
+# )
 
-# Define your tasks here
-tasks_list = [tasks.__getattribute__(name)() for name in task_names]
 
-for task in tasks_list:
-    for agent in agents_list:
-        tasks_list.append(tasks(agent, genes))
 
+# do 10 iterations 
 
 crew = Crew(
-    agents=agents_list,
-    tasks=tasks_list,
+    agents=cell_bio_agent,
+    tasks=cell_cyc_task,
     verbose=True,
-    output_log_file='/Users/mildredmorales-paredes/Ai-Agent/Results/var_log.txt', # or could just say true 
+    output_log_file='/Users/mildredmorales-paredes/Ai-Agent/Results/task_1_log.txt', # or could just say true 
 )
 
-
+# print("This log file looks at the cell cycle task done by the cellular bio agent")
+# for x in range(11):
 result = crew.kickoff()
+print("NEW ITERATION STARTS HERE")
 print(result)
+
 
 
 # Accessing the task output
