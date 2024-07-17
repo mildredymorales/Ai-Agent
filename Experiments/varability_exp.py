8b6e37cbfedd773261e738deec0110329644a1aa
mildredymorales <mmoralesparedes@g.hmc.edu>
2024-07-17 15:17:40 -0400

    fixed range, did cell cycle task with cell bio agent

--- Experiments/varability_exp.py
+++ Experiments/varability_exp.py
@@ -25,27 +25,20 @@ print('-------------------------------')
 # agent = input("What is the the biological agent you want it to specifically analyze as? Please enter with NO quotes. For now it is sensitive so enter the exact function name.\n")
 # task = input("What is the the biological terms you want it to specifically analyze as? Please enter with NO quotes. For now it is sensitive so enter the exact function name\n")
 
-
-
-
 genes = 'GBP5, OASL, ANKRD22, AGR2, LGALS4, KIF14, KIF20A, KIF18B, DLGAP5, TROAP, DEPDC1, PRR11'
 
-# # agent = getattr(agents, agent)
-# # task = getattr(tasks, task)
-# agent = getattr(agents, agent)
-# task = getattr(tasks, task)(agent, genes)
 
 # Define your custom agents and tasks here
 cell_bio_agent = agents.cellular_biologist()
-comp_bio_agent = agents.computational_biologist()
-cell_cyc_agent = agents.cell_cyc()
-drug_dev_agent = agents.drug_dev()
-epig_bio_agent = agents.epigenetics_biologist()
-gene_reg_agent = agents.genereg_expert()
-molec_bio_agent = agents.molecular_biologist()
-onco_res_agent = agents.onco_res()
-onco_phy_agent = agents.oncologist_physician()
-sys_bio_agent = agents.systems_biologist()
+# comp_bio_agent = agents.computational_biologist()
+# cell_cyc_agent = agents.cell_cyc()
+# drug_dev_agent = agents.drug_dev()
+# epig_bio_agent = agents.epigenetics_biologist()
+# gene_reg_agent = agents.genereg_expert()
+# molec_bio_agent = agents.molecular_biologist()
+# onco_res_agent = agents.onco_res()
+# onco_phy_agent = agents.oncologist_physician()
+# sys_bio_agent = agents.systems_biologist()
 
 
 cell_cyc_task = tasks.cell_cycle(
@@ -84,17 +77,17 @@ cell_cyc_task = tasks.cell_cycle(
 # do 10 iterations 
 
 crew = Crew(
-    agents=cell_bio_agent,
-    tasks=cell_cyc_task,
+    agents=[cell_bio_agent],
+    tasks=[cell_cyc_task],
     verbose=True,
     output_log_file='/Users/mildredmorales-paredes/Ai-Agent/Results/task_1_log.txt', # or could just say true 
 )
 
-# print("This log file looks at the cell cycle task done by the cellular bio agent")
-# for x in range(11):
-result = crew.kickoff()
-print("NEW ITERATION STARTS HERE")
-print(result)
+print("This log file looks at the cell cycle task done by the cellular bio agent")
+for x in range(10):
+    result = crew.kickoff()
+    print("NEW ITERATION STARTS HERE")
+    print(result)
 
 
 
