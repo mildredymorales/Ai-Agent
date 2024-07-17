84bfa187f6d21b1a6974fbcec5314bed57b3157a
mildredymorales <mmoralesparedes@g.hmc.edu>
2024-07-17 11:03:40 -0400

    get necessary tasks for this branch

--- Source_Code/tasks.py
+++ Source_Code/tasks.py
@@ -1,41 +1,111 @@
 from crewai import Task
 
 class BiologicalAnalysisTask():
-    def summary(self, agent, genes): 
+    def term(self, agent, genes, term):
         return Task(
-            description = f"write a summary based on the biologicl analysis done on the '{genes}, write a paragraph for each gene in the list.'",
-            agent = agent,
-            expected_output = "a couple of paragraphs words",
-        )
-
-    def review_summary(self, agent, genes):
-        return Task(
-            description = f"""review, edit, and fact-check the summary that explains the gene's relationship and function to each other based on the research '{genes}'. structure the summary in an efficient way. make sure there is 
-            enough context for biologists of all backgrounds. include author and date for the information gathered""",
-            agent = agent,
-            expected_output = "300 words",
-        )
-    
-    def hypothesize(self, agent, genes):
-        return Task(
-            description = f"""your mission is to conduct biological analysis of the '{genes}' based on the information already given to you. Create a hypothesis of these genes as a whole clade in regards to the cell cycle and other 
-            context if a gene isn't relevant to the cell cycle. Hypothesize their relationship, interaction, significance, context  within your respective fields. Give a biological mechanism for this clade.""",
+            description = f"""Your task is to conduct a comprehensive biological analysis of the {genes} based on your expertise. Gather detailed information on the role of each gene in each of 
+            the biological process included in each of these terms: {term}, how these genes interact with each other, the pathways or mechanisms they are involved in, and relevant research findings and studies that 
+            highlight their functions and interactions.""",
             agent = agent,
             expected_output = "a couple of paragraphs",
         )
-    
-    def info_dump(self, agent, genes):
+    def immune_response(self, agent, genes, term):
         return Task(
-            description = f"""your mission is to gather all the biological, enrichment, context dependent information on the '{genes}' that can be found. Give the author, date, and other references for where the information is given. 
-            Even if there is no information on the gene, keep exploring and note the limited information.""",
+            description = f"""I will provide you with {genes}.
+            "your task is to conduct a comprehensive biological analysis based on your expertise.
+            "Gather detailed information on the role of each gene in immune response,
+            how these genes interact with each other, the pathways or mechanisms they are involved in, 
+            and relevant research findings and studies that highlight their functions and interactions.""",
             agent = agent,
-            expected_output = "1000 words",
+            expected_output = "500 word count of biological information based on the immune response information on the genes",
         )
-    
-    def compare_findings(self, agent):
+    def metabolic_process(self, agent, genes):
         return Task(
-            description = f"""Your mission is to analyze and compare the conclusions drawn by each of the 10 biological agents that previously analyzed the clade of genes. Based on their analysis of the genetic data give a percentage 
-            of how similar each hypothesis is and rank each of the agents. In the analysis focus including the style and manner in which the information is presented by each agent.""",
+            description = f"""I will provide you with {genes}.
+            "your task is to conduct a comprehensive biological analysis based on your expertise."
+            "Gather detailed information on the role of each gene in metabolic process, how these genes interact with each other, 
+            the pathways or mechanisms they are involved in, 
+            and relevant research findings and studies that highlight their functions and interactions.""",
             agent = agent,
-            expected_output = "a couple of paragraphs",
+            expected_output = "500 word count of biological information based on the metabolic process information on the genes",
+        )
+    def cell_cycle(self, agent, genes):
+        return Task(
+            description = f"""I will provide you with {genes}.
+            "your task is to conduct a comprehensive biological analysis based on your expertise."
+            "Gather detailed information on the role of each gene in the cell cycle, 
+            how these genes interact with each other, the pathways or mechanisms they are involved in, 
+            and relevant research findings and studies that highlight their functions and interactions.""",
+            agent = agent,
+            expected_output = "500 word count of biological information based on the cell cycle information on the genes",
+        )
+    def cell_comm(self, agent, genes):
+        return Task(
+            description = f"""I will provide you with {genes}.
+            "your task is to conduct a comprehensive biological analysis based on your expertise."
+            "Gather detailed information on the role of each gene in cell communication, 
+            how these genes interact with each other, the pathways or mechanisms they are involved in, 
+            and relevant research findings and studies that highlight their functions and interactions.""",
+            agent = agent,
+            expected_output = "500 word count of biological information based on the cell communication information on the genes",
+        )
+    def signal_trans(self, agent, genes):
+        return Task(
+            description = f"""I will provide you with {genes}.
+            "your task is to conduct a comprehensive biological analysis based on your expertise."
+            "Gather detailed information on the role of each gene in signal transduction, 
+            how these genes interact with each other, the pathways or mechanisms they are involved in, 
+            and relevant research findings and studies that highlight their functions and interactions.""",
+            agent = agent,
+            expected_output = "500 word count of biological information based on the signal transduction information on the genes",
+        )
+    def apop(self, agent, genes):
+        return Task(
+            description = f"""I will provide you with {genes}.
+            "your task is to conduct a comprehensive biological analysis based on your expertise.
+            "Gather detailed information on the role of each gene in apoptosis, 
+            how these genes interact with each other, the pathways or mechanisms they are involved in,
+            and relevant research findings and studies that highlight their functions and interactions.""",
+            agent = agent,
+            expected_output = "500 word count of biological information based on the apoptosis information on the genes",
+        )
+    def develop(self, agent, genes):
+        return Task(
+            description = f"""I will provide you with {genes}.
+            "your task is to conduct a comprehensive biological analysis based on your expertise.
+            "Gather detailed information on the role of each gene in development, 
+            how these genes interact with each other, the pathways or mechanisms they are involved in, 
+            and relevant research findings and studies that highlight their functions and interactions.""",
+            agent = agent,
+            expected_output = "500 word count of biological information based on the development information on the genes",
+        )
+    def repro(self, agent, genes):
+        return Task(
+            description = f"""I will provide you with {genes}.
+            "your task is to conduct a comprehensive biological analysis based on your expertise.
+            "Gather detailed information on the role of each gene in reproduction,
+            how these genes interact with each other, the pathways or mechanisms they are involved in, 
+            and relevant research findings and studies that highlight their functions and interactions.""",
+            agent = agent,
+            expected_output = "500 word count of biological information based on the reproduction information on the genes",
+        )
+    def transport(self, agent, genes):
+        return Task(
+            description = f"""I will provide you with {genes}.
+            "your task is to conduct a comprehensive biological analysis based on your expertise.
+            "Gather detailed information on the role of each gene in transport,
+            how these genes interact with each other, the pathways or mechanisms they are involved in, 
+            and relevant research findings and studies that highlight their functions and interactions.""",
+            agent = agent,
+            expected_output = "500 word count of biological information based on the gene transport information on the genes",
+        )
+    def reg_biop(self, agent, genes):
+        return Task(
+            description = f"""I will provide you with {genes}.
+            "your task is to conduct a comprehensive biological analysis based on your expertise."
+            "Gather detailed information on the role of each gene in regulation of biological processes, 
+            how these genes interact with each other, the pathways or mechanisms they are involved in, 
+            and relevant research findings and studies that highlight their functions and interactions.""",
+            agent = agent,
+            expected_output = "500 word count of biological information based on the regulation of biological processes information on the genes",
         )
