216c8d1a3a7a12422af037412eb343dba5e2cd4c
selnsimay <sskaplanoglu@gmail.com>
2024-07-24 17:51:13 -0400

    one iteration-10 agent task

--- Experiments/one_iteration_ex.py
+++ Experiments/one_iteration_ex.py
@@ -1,5 +1,6 @@
-from crewai import Crew, Process, Agent
+from crewai import Crew, Task, Agent
 from langchain_community.llms import Ollama
+
 model = Ollama(model='llama3')
 
 from Source_Code.agents import BiologicalAgents
@@ -9,11 +10,10 @@ from Source_Code.tasks import BiologicalAnalysisTask
 agents = BiologicalAgents()
 tasks = BiologicalAnalysisTask()
 
-task_name= input("What is the the biological term you want it to specifically analyze as? Please enter with NO quotes. For now it is sensitive so enter the exact function name\n")
+task_name = input("What is the biological term you want it to specifically analyze as? Please enter with NO quotes. For now it is sensitive so enter the exact function name\n")
 
 # Define the list of genes
 genes = 'GBP5, OASL, ANKRD22, AGR2, LGALS4, KIF14, KIF20A, KIF18B, DLGAP5, TROAP, DEPDC1, PRR11'
-
 
 # List of agent names
 agent_names = [
@@ -25,24 +25,17 @@ agent_names = [
 # Create instances of the agents
 agents_list = [getattr(agents, name)() for name in agent_names]
 
-# Define the task
-tasks_list = []
-
-# could change the actual tasks to the term task for human input 
-# Append tasks for each agent
-for agent in agents_list:
-    tasks_lists = getattr(tasks, task_name)(agent, genes)
+# Create a list of tasks
+tasks_list = [getattr(tasks, task_name)(agent, genes) for agent in agents_list]
 
 # Create the Crew
 crew = Crew(
     agents=agents_list,
     tasks=tasks_list,
     verbose=True,
-    output_log_file=f"/Users/n4k/Ai-Agent/Results/{task_name}_one_iteration.txt",
+    output_log_file=f"/Users/n4k/Ai-Agent/Results/one_iteration/{task_name}.txt",
 )
 
 # Kick off the tasks
 result = crew.kickoff()
 print(result)
-
-
