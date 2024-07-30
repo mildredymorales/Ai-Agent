6bfa6dd50778795f03ae77d7b593de5c0bc865ca
mildredymorales <mmoralesparedes@g.hmc.edu>
2024-07-30 14:42:00 -0400

    task exp update

--- Experiments/task_example.py
+++ Experiments/task_example.py
@@ -10,26 +10,28 @@ genes = 'INSM1, HES1, STK17B, TRIB1, HS3ST1, DSC2, SLC12A1, HECTD2, OPRM1, NEURO
  
 research_agent = Agent(
     role = 'researcher',
-    goal = "based on the list of genes, provide the significance of the gene's relationship and function to each other based on researched biological data",
-    backstory = 'You are an confident biologist scientist who studies human genes and disease. You are yo analyze sets of genes. ',
+    goal = """based on the list of genes, provide the significance of the gene's relationship 
+    and function to each other based on researched biological data.""",
+    backstory = """You are an confident biologist scientist who studies human genes and disease. 
+    You are to analyze sets of genes.""",
     allow_delegation = False,
     llm = model
 )
-
-bad_task = Task(
-    description = f"""Analyze a bunch of '{genes}' and figure out if they are important. Try to see if they are related to each other somehow. 
-    Write a summary of what you discovered about the genes and their connections.""",
+short_task = Task(
+    description = f"""Analyze a bunch of '{genes}' and figure out if they are important. 
+    Try to see if they are related to each other somehow.""",
     agent = research_agent,
     expected_output = "a couple of paragraphs",
 )
-
-good_task = Task(
-    description = f"""The objective of this task is to conduct a comprehensive analysis of a specified '{genes}' to elucidate its biological significance and identify potential 
-    relationships within the context of a given biological framework. Perform functional enrichment analyses to interpret the biological roles of the genes. 
-    This includes Gene Ontology (GO) enrichment to ascertain which biological processes, molecular functions, and cellular components are overrepresented. Additionally, 
-    conduct pathway analysis using databases like KEGG or Reactome to map the DEGs to known biological pathways, thereby elucidating their potential roles in specific 
-    biological processes or diseases. Integrate these findings to explore the relationships between the genes, looking at how they interact within the pathways and processes identified.
-    The final deliverable should provide a thorough understanding of the gene set's biological relevance and its implications for further research or clinical applications.""",
+long_task = Task(
+    description = f"""The objective of this task is to perform a detailed analysis of the 
+    {genes}`to uncover its biological significance and identify potential relationships 
+    within a defined biological framework. This involves examining specific gene interactions, 
+    mapping these genes to relevant biological pathways, and exploring their roles in cellular 
+    processes. Additionally, the analysis should include hypotheses about less understood or 
+    novel interactions. The final deliverable should be a comprehensive report that 
+    integrates these findings, providing a thorough understanding of the genes biological 
+    relevance""",
     agent = research_agent,
     expected_output = "a report with multiple pages",
 )
@@ -38,7 +40,7 @@ good_task = Task(
  
 crew = Crew(
     agents = [research_agent],
-    tasks = [good_task],
+    tasks = [short_task],
     verbose = True,
     process = Process.sequential, 
     output_log_file=True,
